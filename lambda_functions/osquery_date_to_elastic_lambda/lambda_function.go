96dc960f8fae70e5bde8dc1cfd1b29fa313fc8c1
Ryan Deivert <ryandeivert@gmail.com>
2018-02-06 22:39:37 -0800

    [lambda] converting the date transformation lambda from python to go

---
+++ lambda_functions/osquery_date_to_elastic_lambda/lambda_function.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/ryandeivert/sgt/logger"
+)
+
+func convertData(date string) (string, error) {
+
+	oldDate := strings.TrimRight(date, " UTC")
+	t, err := time.Parse(time.ANSIC, oldDate)
+	if err != nil {
+		return "", err
+	}
+
+	// Return the properly formatted date
+	return t.Format("2006-01-02T15:04:05"), nil
+}
+
+func transform(rec events.KinesisFirehoseEventRecord) ([]byte, error) {
+
+	recordData := make([]byte, base64.StdEncoding.DecodedLen(len(rec.Data)))
+	_, err := base64.StdEncoding.Decode(recordData, rec.Data)
+	if err != nil {
+		return rec.Data, err
+	}
+
+	var data map[string]interface{}
+	dec := json.NewDecoder(strings.NewReader(string(recordData)))
+	err = dec.Decode(&data)
+	if err != nil {
+		return rec.Data, err
+	}
+
+	calTime := data["calendarTime"].(string)
+	newCalTime, err := convertData(calTime)
+	if err != nil {
+		return rec.Data, err
+	}
+
+	data["calendarTime"] = newCalTime
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return rec.Data, err
+	}
+
+	newData := make([]byte, base64.StdEncoding.EncodedLen(len(rec.Data)))
+	base64.StdEncoding.Encode(newData, jsonData)
+
+	return newData, nil
+}
+
+// Handler is the main AWS Lambda entry point
+func Handler(event events.KinesisFirehoseEvent) (events.KinesisFirehoseResponse, error) {
+	var results events.KinesisFirehoseResponse
+
+	var failed int
+
+	for _, record := range event.Records {
+
+		responseRec := events.KinesisFirehoseResponseRecord{
+			RecordID: record.RecordID,
+		}
+
+		var err error
+		responseRec.Data, err = transform(record)
+		if err == nil {
+			responseRec.Result = events.KinesisFirehoseTransformedStateOk
+		} else {
+			logger.Error("could not transform record:", err)
+			responseRec.Result = events.KinesisFirehoseTransformedStateProcessingFailed
+			failed++
+		}
+
+		// Append the record to the list of records
+		results.Records = append(results.Records, responseRec)
+	}
+
+	fmt.Printf("Processing completed. Successful records %d, Failed records %d.\n",
+		len(results.Records)-failed, failed)
+
+	return results, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
---
+++ lambda_functions/osquery_date_to_elastic_lambda/lambda_function_test.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"encoding/base64"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+func TestConvertDate01(t *testing.T) {
+	dateString := "Mon Oct 30 03:00:41 2017 UTC"
+	d, err := convertData(dateString)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if d != "2017-10-30T03:00:41" {
+		t.Error("Invalid date conversion")
+	}
+}
+
+func TestConvertDate02(t *testing.T) {
+	dateString := "Tue Sep 30 17:37:30 2014"
+	d, err := convertData(dateString)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if d != "2014-09-30T17:37:30" {
+		t.Error("Invalid date conversion")
+	}
+}
+
+func TestConvertDateInvalid(t *testing.T) {
+	dateString := "2014-09-30T17:37:30"
+	_, err := convertData(dateString)
+	if err == nil {
+		t.Error("ANSIC date conversion should fail")
+	}
+}
+
+func TestHandlerValid(t *testing.T) {
+	data := `{
+  "action": "removed",
+  "columns": {
+    "name": "osqueryd",
+    "path": "/usr/local/bin/osqueryd",
+    "pid": "97650"
+  },
+  "name": "processes",
+  "hostname": "hostname.local",
+  "calendarTime": "Tue Sep 30 17:37:30 2014 UTC",
+  "unixTime": "1412123850"
+}`
+
+	dataPayload := make([]byte, base64.StdEncoding.EncodedLen(len(data)))
+	base64.StdEncoding.Encode(dataPayload, []byte(data))
+
+	event := events.KinesisFirehoseEvent{
+		InvocationID:      "01960bf1-64cb-4e41-bbc0-92fa5b14665e",
+		DeliveryStreamArn: "arn:aws:firehose:us-east-1:123456789012:deliverystream/delivery-stream-name",
+		Region:            "us-east-1",
+		Records: []events.KinesisFirehoseEventRecord{
+			events.KinesisFirehoseEventRecord{
+				RecordID:                    "001",
+				ApproximateArrivalTimestamp: events.MilliSecondsEpochTime{time.Now()},
+				Data: dataPayload,
+			},
+		},
+	}
+
+	results, _ := Handler(event)
+
+	if len(results.Records) != 1 {
+		t.Errorf("Expected 1 record, got %d records", len(results.Records))
+	}
+
+	if results.Records[0].Result != events.KinesisFirehoseTransformedStateOk {
+		t.Error("Expected successful record")
+	}
+}
+
+func TestHandlerInvalid(t *testing.T) {
+	data := `{
+  "action": "removed",
+  "columns": {
+    "name": "osqueryd",
+    "path": "/usr/local/bin/osqueryd",
+    "pid": "97650"
+  },
+  "name": "processes",
+  "hostname": "hostname.local",
+  "calendarTime": "2014-09-30T17:37:30",
+  "unixTime": "1412123850"
+}`
+
+	dataPayload := make([]byte, base64.StdEncoding.EncodedLen(len(data)))
+	base64.StdEncoding.Encode(dataPayload, []byte(data))
+
+	event := events.KinesisFirehoseEvent{
+		InvocationID:      "01960bf1-64cb-4e41-bbc0-92fa5b14665e",
+		DeliveryStreamArn: "arn:aws:firehose:us-east-1:123456789012:deliverystream/delivery-stream-name",
+		Region:            "us-east-1",
+		Records: []events.KinesisFirehoseEventRecord{
+			events.KinesisFirehoseEventRecord{
+				RecordID:                    "001",
+				ApproximateArrivalTimestamp: events.MilliSecondsEpochTime{time.Now()},
+				Data: dataPayload,
+			},
+		},
+	}
+
+	results, _ := Handler(event)
+
+	if len(results.Records) != 1 {
+		t.Errorf("Expected 1 record, got %d records", len(results.Records))
+	}
+
+	if results.Records[0].Result != events.KinesisFirehoseTransformedStateProcessingFailed {
+		t.Error("Expected failed record")
+	}
+}
