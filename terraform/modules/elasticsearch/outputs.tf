01fdd7290968265eca01005e93f166859b8226bd
EC2 Default User <ec2-user@ip-172-31-9-140.ec2.internal>
2018-07-30 22:42:50 +0000

    I added cognito support for Kibana dashboard

--- terraform/modules/elasticsearch/outputs.tf
+++ terraform/modules/elasticsearch/outputs.tf
@@ -5,3 +5,23 @@ output "elasticsearch_domain_arn" {
 output "elasticsearch_endpoint" {
   value = "${aws_elasticsearch_domain.sgt-osquery_results.endpoint}"
 }
+
+output "elasticsearch_domain_id" {
+  value = "${aws_elasticsearch_domain.sgt-osquery_results.domain_name}"
+}
+
+output "cognito_user_pool_id" {
+  value = "${aws_cognito_user_pool.pool.id}"
+}
+
+output "cognito_identity_pool_id" {
+  value = "${aws_cognito_identity_pool.main.id}"
+}
+
+output "elasticsearch_cognito_role_arn" {
+  value = "${aws_iam_role.es_cognito_access_role.arn}"
+}
+
+output "elasticsearch_region" {
+  value = "us-east-1"
+}
--- terraform/modules/elasticsearch/variables.tf
+++ terraform/modules/elasticsearch/variables.tf
@@ -4,6 +4,16 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
+variable "user_pool_name" {
+  default = "kibana_user_pool"
+}
+variable "identity_pool_name" {
+  default = "kibana_identity_pool"
+}
+variable "es_cognito_access_role_name" {
+  default = "es_cognito_role"
+}
+
 variable "elasticsearch_domain_name" {
   default = "sgt-osquery-results"
 }
